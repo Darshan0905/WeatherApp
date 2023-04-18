4960b7a176ab154cef24ec7a5d11053cf8cfb5d3
darshan munjapara <hardik>
2023-04-18 11:52:24 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Weather from './components/Weather';
+import { Dimmer, Loader } from 'semantic-ui-react';
+
+const URL= `https://api.openweathermap.org/data/2.5/weather`
+const API_KEY = `9c74fe25b8858f559e638b0dc7750540`
 
 function App() {
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        setLat(position.coords.latitude);
+        setLong(position.coords.longitude);
+      });
+
+      await fetch(`${URL}?lat=${lat}&lon=${long}&appid=${API_KEY}`)
+        .then(weatherData => weatherData.json())
+        .then(weatherData => {
+          setData(weatherData)
+          console.log(weatherData,"weatherData");
+        });
+    }
+    fetchData();
+  }, [lat, long])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {(typeof data.main != 'undefined') ? (  
+        <Weather weatherData={data} />
+      ) : (
+        <div>
+          {/* <Dimmer active>
+            <Loader>Loading..</Loader>
+          </Dimmer> */}
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/components/Weather.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './styles.css';
+import { Card } from 'semantic-ui-react'
+import moment from 'moment';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCloud } from '@fortawesome/free-solid-svg-icons'
+    // import { Button } from 'semantic-ui-react';
+
+    // const refresh = () => {
+    //   window.location.reload();
+    // }
+
+
+const WeatherCard = ({weatherData}) => (
+    <div className="body">
+    <div className="main">
+      <div className="top">
+        <p className="header">{weatherData.name}</p>
+        <p className='header'><FontAwesomeIcon icon={faCloud} /></p>
+        {/* <Button className="button" inverted color='blue' circular icon='refresh' onClick={refresh} /> */}
+      </div>
+      <div className="flex">
+        <p className="day">{moment().format('dddd')}, <span>{moment().format('LL')}</span></p>
+        <p className="description">{weatherData.weather[0].main}</p>
+      </div>
+
+      <div className="flex">
+        <p className="temp">Temprature: {weatherData.main.temp} &deg;C</p>
+        <p className="temp">Humidity: {weatherData.main.humidity} %</p>
+      </div>
+
+      <div className="flex">
+        <p className="sunrise-sunset">Sunrise: {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString('en-IN')}</p>
+        <p className="sunrise-sunset">Sunset: {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString('en-IN')}</p>
+      </div>
+      </div>
+  </div>
+  )
+  
+  export default WeatherCard;
